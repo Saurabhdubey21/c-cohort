a4449076ce3430911a72fe7470dd31c42b557e53
Saurabhdubey21 <mkd221524@gmail.com>
2025-08-25 23:30:12 +0530

    day 3

---
+++ bitwiseoperators/2nd.cpp
@@ -0,0 +1,10 @@
+// post increment and pre increament
+#include<iostream>
+using namespace std;
+int main(){
+    int i=7;
+    cout<<(++i)<<endl;
+    cout<<(i++)<<endl;
+    cout<<(i--)<<endl;
+    cout<<(--i)<<endl;
+}
