5d014843d2a009e45dc8eb646ba4fbd9cf105cc5
Saurabhdubey21 <mkd221524@gmail.com>
2025-08-26 00:32:46 +0530

    day3

---
+++ bitwiseoperators/7th.cpp
@@ -0,0 +1,10 @@
+// continue
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<=5;i++){
+        cout<<"hey"<<endl;
+        cout<<"hi"<<endl;
+        cout<<"reply asap"<<endl;
+    }
+}
