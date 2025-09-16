97742a8bf015e5fac9ff727eccee10e4e8cfdfc7
Saurabhdubey21 <mkd221524@gmail.com>
2025-09-16 19:45:08 +0530

    c++

---
+++ day 2/4th.cpp
@@ -0,0 +1,26 @@
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+            // for space
+            for(int j=1;j<=n-i;j++){
+                cout<<" ";
+            }
+            // star
+            for(int j=1;j<=2*i-1;j++){
+                cout<<"*";
+            }
+            // space
+            for(int j=1;j<=n-i;j++){
+                cout<<" ";
+            }
+        cout<<endl;
+    }
+}
---
+++ day 2/5th.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for (int j=1;j<=i;j++){
+            char ch='A';
+            ch=ch+i-1;
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
