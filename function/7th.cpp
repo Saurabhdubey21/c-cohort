683f663bddd33fa90a96a659047981bd8334a10c
Saurabhdubey21 <mkd221524@gmail.com>
2025-09-19 22:52:40 +0530

    functions

---
+++ function/7th.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+float sum(float a,int b){
+    return a+b;
+}
+int main(){
+    cout<<sum(72.8,9.1);
+}
+//here we will get our output in decimal value because we have 
+//taken the value of a in float
+//output:81.8
---
+++ function/8th.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    cout<<sqrt(4)<<endl;
+    cout<<cbrt(729)<<endl;
+    cout<<min(89,76)<<endl;
+    cout<<max(98,898)<<endl;
+    cout<<pow(2,6)<<endl;
+}
+//output:
+// 2
+// 9
+// 76
+// 898
+// 64
